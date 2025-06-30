4dab7ad2b1d46205814f63c7824cae6c6e6a5b25
safronCode <justfugue@gmail.com>
2025-07-01 02:54:00 +0300

    Commit 5: Добавил иконки (SVG) для фронта. Удалил дефолтные с установки.

---
+++ frontend/src/components/icons/IconEmptyLike.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" baseProfile="full" width="90px" height="90px" viewBox="0 0 90 90">
+  <rect x="0" y="20" width="10" height="10" fill="#000000" />
+  <rect x="0" y="30" width="10" height="10" fill="#000000" />
+  <rect x="0" y="40" width="10" height="10" fill="#000000" />
+  <rect x="10" y="10" width="10" height="10" fill="#000000" />
+  <rect x="10" y="50" width="10" height="10" fill="#000000" />
+  <rect x="20" y="0" width="10" height="10" fill="#000000" />
+  <rect x="20" y="60" width="10" height="10" fill="#000000" />
+  <rect x="30" y="0" width="10" height="10" fill="#000000" />
+  <rect x="30" y="70" width="10" height="10" fill="#000000" />
+  <rect x="40" y="10" width="10" height="10" fill="#000000" />
+  <rect x="40" y="80" width="10" height="10" fill="#000000" />
+  <rect x="50" y="0" width="10" height="10" fill="#000000" />
+  <rect x="50" y="70" width="10" height="10" fill="#000000" />
+  <rect x="60" y="0" width="10" height="10" fill="#000000" />
+  <rect x="60" y="60" width="10" height="10" fill="#000000" />
+  <rect x="70" y="10" width="10" height="10" fill="#000000" />
+  <rect x="70" y="50" width="10" height="10" fill="#000000" />
+  <rect x="80" y="20" width="10" height="10" fill="#000000" />
+  <rect x="80" y="30" width="10" height="10" fill="#000000" />
+  <rect x="80" y="40" width="10" height="10" fill="#000000" />
+  </svg>
+</template>
